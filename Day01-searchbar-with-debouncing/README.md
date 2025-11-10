a6302ae5966229781bf19b7efa4bcd43be214a59
Subham0813 <subham913dces3@gmail.com>
2025-11-10 23:02:45 +0530

    refactored code: final commit

--- Day01-searchbar-with-debouncing/README.md
+++ Day01-searchbar-with-debouncing/README.md
@@ -1,16 +1,34 @@
-# React + Vite
+# Autocomplete Search App (React + Tailwind)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A simple autocomplete search bar built using **React**, **Tailwind CSS**, and **React Icons**.
 
-Currently, two official plugins are available:
+### ✨ Main learning
+- Debounced search suggestions, Implemented realusecase of debouncing.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) (or [oxc](https://oxc.rs) when used in [rolldown-vite](https://vite.dev/guide/rolldown)) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## 🚀 Setup & Run Locally
 
-## React Compiler
-
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+### Clone the repository
+```bash
+git clone https://github.com/subham0813/Day01-searchbar-with-debouncing
+```
+- Install dependencies
+```bash
+npm install
+```
+- If Tailwind is not installed yet, run:
+```bash
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+```
+- Update tailwind.config.js like this:
+```js
+content: ["./src/**/*.{js,jsx,ts,tsx}"],
+```
+### Start Development Server
+```bash
+ npm run dev   # for Vite 
+```
+## or
+```bash
+npm start     # for CRA
+```
--- Day01-searchbar-with-debouncing/src/App2.jsx
+++ Day01-searchbar-with-debouncing/src/App2.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MdClose, MdSearch } from "react-icons/md";
 
 const suggestionsList = [
@@ -30,63 +30,80 @@ const mySugesstions = ([str, setFn]) => {
   setFn(res);
 };
 
-const getSuggestions = debounce(mySugesstions, 500);
+const getSuggestions = debounce(mySugesstions, 200);
 
 function App() {
   const [inputval, setInputval] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickedOutside = (e) => {
+      if (wrapperRef.current && !wrapperRef.current.contains(e.target))
+        setSuggestions([]);
+      // else getSuggestions(e.target.value, setSuggestions);
+    };
+
+    window.document.addEventListener("mousedown", handleClickedOutside);
+    return () =>
+      window.document.removeEventListener("mousedown", handleClickedOutside);
+  }, []);
+
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-start p-6 bg-gray-100 font-mono">
-      <div className="flex items-center gap-2 w-full max-w-md bg-white p-3 shadow rounded-xl">
-        <input
-          type="text"
-          name="searchbar-main"
-          id="searchbar-main"
-          placeholder="Type to search..."
-          value={inputval}
-          onChange={(e) => {
-            setInputval(e.target.value);
-            getSuggestions(e.target.value, setSuggestions);
-          }}
-          className="flex-1 outline-none text-gray-700 text-lg px-2"
-        />
-        {inputval ? <button className="text-gray-600 hover:text-gray-800 text-2xl pl-2" onClick={() => setInputval('')}>
-            <MdClose/>
-        </button>: ''}
-
-        <button className="text-gray-600 hover:text-blue-500 text-2xl pl-2">
-          <MdSearch />
-        </button>
-      </div>
-
-      <div className="w-full max-w-md mt-2 bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
-        {suggestions.map((item) => (
-          <div
-            onClick={() => setInputval(item)}
-            key={item}
-            className="flex justify-between items-center px-4 py-2  last:border-none hover:bg-blue-50 transition"
-          >
-            <p
-              className="text-gray-700 font-medium hover:text-blue-600"
-              onClick={() => setInputval(item)}
-            >
-              {item}
-            </p>
+    <div className="w-full min-h-screen flex flex-col items-center justify-start p-6 bg-gray-200 font-mono ">
+      <div
+        className="flex flex-col items-center w-full max-w-md bg-white py-2 shadow rounded-2xl hover:shadow-lg text-xl overflow-hidden"
+        ref={wrapperRef}
+      >
+        <div className="w-full max-w-md flex px-2">
+          <button className="text-gray-600 hover:text-blue-500 pl-2">
+            <MdSearch />
+          </button>
+          <input
+            type="text"
+            name="searchbar-main"
+            id="searchbar-main"
+            placeholder="Type to search..."
+            value={inputval}
+            onChange={(e) => {
+              setInputval(e.target.value);
+              getSuggestions(e.target.value, setSuggestions);
+            }}
+            className="flex-1 outline-none text-gray-700 text-lg px-2 w-full"
+          />
+          {inputval ? (
             <button
+              className="text-gray-600 hover:text-gray-800 text-2xl pr-2"
               onClick={() => {
-                const r = suggestions.filter((sugg) => item !== sugg);
-                setSuggestions(r);
+                setInputval("");
               }}
-              className="text-gray-500 hover:text-red-500 text-xl"
             >
               <MdClose />
             </button>
+          ) : (
+            ""
+          )}
+        </div>
+        {suggestions.length > 0 && (
+          <div className="w-full max-w-md mt-2 cursor-pointer border-t-2 border-gray-200 overflow-hidden">
+            {suggestions.map((item) => (
+              <div
+                key={item}
+                className="w-full flex justify-between gap-2 items-center px-4 py-2 last:border-none hover:bg-blue-50 transition overflow-hidden"
+              >
+                <button className="text-gray-600">
+                  <MdSearch />
+                </button>
+                <p
+                  className="w-full text-gray-600 font-medium hover:text-gray-900"
+                  onClick={() => setInputval(item)}
+                >
+                  {item}
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
-
-        {inputval && suggestions.length === 0 && (
-          <p className="p-3 text-gray-500 text-center">No suggestions</p>
         )}
       </div>
     </div>
